aa5636b5bc3a07272fb57d70172e5f053e9f7788
dp <dp@>
2022-09-04 21:56:34 +0530

    update

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,22 +1,13 @@
 import {gql, useQuery} from '@apollo/client'
 import ClientRow from "./ClientRow";
-
-const GET_CLIENTS = gql`
-query getClients{
-    clients{
-        id
-        name
-        email
-        phone
-    }
-}
-`
+import Spinner from "./Spinner";
+import {GET_CLIENTS} from "../queries/clientQuery";
 
 export default function Clients(props) {
     const {data, loading, error} = useQuery(GET_CLIENTS)
 
-    if(loading) return <div>Loading...</div>
-    if(error) return <div>Couldn't fetch in error</div>
+    if (loading) return <Spinner/>
+    if (error) return <div>Couldn't fetch in error</div>
     return <>
         <table className='table table-hover mt-3'>
             <thead>
@@ -27,8 +18,8 @@ export default function Clients(props) {
                 <td>...</td>
             </tr>
             </thead>
-            {data.clients.map(client=>{
-                return <ClientRow key={client.id} client={client} />
+            {data.clients.map(client => {
+                return <ClientRow key={client.id} client={client}/>
             })}
         </table>
     </>
